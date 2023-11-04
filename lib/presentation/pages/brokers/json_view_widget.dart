58628fc497f261019ae6f0d62b45baefabef74c8
yodainferno <p@namoy.ru>
2023-11-04 17:56:40 +0300

    feat: pick key in json-viewer, open/close nested

--- lib/presentation/pages/brokers/json_view_widget.dart
+++ lib/presentation/pages/brokers/json_view_widget.dart
@@ -1,92 +1,59 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_json_view/flutter_json_view.dart';
-
-// class JsonViewWidget extends StatelessWidget {
-//   const JsonViewWidget({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final arguments = (ModalRoute.of(context)?.settings.arguments ?? <String, dynamic>{}) as Map;
-//     final String data = '''{
-//       "a": 123,
-//       "b": 123123,
-//       "d": {
-//         "ooo": "hahaha",
-//         "aaa": "hahaha",
-//         "a2aa": "hahaha"
-//       },
-//       "e": [
-//         1, 2, 3, 4, {
-//         "ooo": "hahaha",
-//         "aaa": "hahaha",
-//         "a2aa": "hahaha"
-//       }
-//       ]
-//     }''';
-//     print(arguments['data']);
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('New Broker'),
-//       ),
-//       body: SingleChildScrollView(
-//         padding: const EdgeInsets.symmetric(
-//           vertical: 30.0,
-//           horizontal: 15.0,
-//         ),
-//         child: JsonView.string(
-//           data,
-//           theme: JsonViewTheme(
-//             backgroundColor: Colors.white,
-//             keyStyle: TextStyle(
-//               color: Colors.blue[500],
-//             ),
-//             doubleStyle: TextStyle(
-//               color: Colors.green[600],
-//             ),
-//             intStyle: TextStyle(
-//               color: Colors.green[600],
-//             ),
-//             stringStyle: TextStyle(
-//               color: Colors.orange[900],
-//             ),
-//             boolStyle: TextStyle(
-//               color: Colors.blue[800],
-//             ),
-//             closeIcon: Icon(
-//               Icons.arrow_drop_up,
-//               color: Colors.grey[900],
-//             ),
-//             openIcon: Icon(
-//               Icons.arrow_drop_down,
-//               color: Colors.grey[900],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class JsonTreeStore extends ChangeNotifier {
+  List _path = [];
+  List get path => _path;
+  set path(List path) {
+    _path = path;
+    notifyListeners();
+  }
+
+  Map<String, bool> _opened_pathes = {};
+  bool isOpened(List path) {
+    return _opened_pathes[path.join('_')] == true;
+  }
+  void open(List path) {
+    _opened_pathes[path.join('_')] = true;
+    notifyListeners();
+  }
+
+  void close(List path) {
+    _opened_pathes[path.join('_')] = false;
+    notifyListeners();
+  }
+}
 
 class JsonViewWidget extends StatelessWidget {
   const JsonViewWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // final arguments = (ModalRoute.of(context)?.settings.arguments ??
-    //     <String, dynamic>{}) as Map;
-    final String data = '''{
-      "a": 123,
-      "b": 123123,
-      "d": {
-        "ooo": "hahaha",
-        "aaa": "hahaha",
-        "a2aa": "hahaha"
-      }
-    }'''; //arguments['data'] ?? '{}';
+    final arguments = (ModalRoute.of(context)?.settings.arguments ??
+        <String, dynamic>{}) as Map;
+    
+    var jsonData;
+    try {
+      throw Error();
+      jsonData = json.decode(arguments['data']);
+    } catch(_) {
+      // jsonData = {};
+      jsonData = {
+        "payload": {
+          "temp": {
+            "value": 22.1,
+            "unit": "C",
+          },
+          "hum": {
+            "value": 780.4,
+            "unit": "ммрст",
+          }
+        },
+        "created": "2023-11-04:10:19:02"
+      };
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -98,17 +65,32 @@ class JsonViewWidget extends StatelessWidget {
             vertical: 30.0,
             horizontal: 30.0,
           ),
-          child: JsonTreeViewNode(
-            jsonData: json.decode(data),
-            path: [],
+          child: MultiProvider(
+            providers: [
+              ChangeNotifierProvider(
+                create: (_) => JsonTreeStore(),
+              ),
+            ],
+            child: Builder(builder: (BuildContext context) {
+              final JsonTreeStore jsonTreeStore =
+              Provider.of<JsonTreeStore>(context, listen: true);
+
+              // print(jsonTreeStore.path);
+
+              return Column(children: [
+                JsonTreeViewNode(
+                jsonData: jsonData,
+                path: [],
+              ), 
+              Text("PATH: ${jsonTreeStore.path}")
+              ]);
+            }),
           ),
         ),
       ),
     );
   }
 }
-
-final path = ["d", "aaa"];
 
 class JsonTreeViewNode extends StatelessWidget {
   final dynamic jsonData;
@@ -119,31 +101,25 @@ class JsonTreeViewNode extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final JsonTreeStore jsonTreeStore =
+        Provider.of<JsonTreeStore>(context, listen: true);
+    bool isOpenedKey = listEquals(path, jsonTreeStore.path) == true;
+
     if (jsonData is Map) {
       final w = Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: List<Widget>.from(
           jsonData.entries.map((entry) {
-            // bool isOpenedKey = path.asMap()[level - 1] == entry.key;
             return IntrinsicHeight(
               child: Row(
                 children: [
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      GestureDetector(
-                        onTap: () {
-                          // print('Key tapped: ${entry.key}');
-                          print(path);
-                        },
-                        child: Text(
-                          '${entry.key}:',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.blue,
-                            // backgroundColor: isOpenedKey ? Colors.red[200] : null
-                          ),
-                        ),
+                      Text(
+                        '${entry.key}:',
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.blue),
                       ),
                       Expanded(
                         child: Container(
@@ -155,9 +131,31 @@ class JsonTreeViewNode extends StatelessWidget {
                     ],
                   ),
                   Padding(
-                    padding: EdgeInsets.only(left: 10.0),
-                    child: JsonTreeViewNode(
-                        jsonData: entry.value, path: [...path, entry.key]),
+                    padding: EdgeInsets.only(left: 2.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                      GestureDetector(
+                        onTap: () {
+                        final path_nested = [...path, entry.key];
+                        if (jsonTreeStore.isOpened(path_nested)) {
+                          jsonTreeStore.close(path_nested);
+                        } else {
+                          jsonTreeStore.open(path_nested);
+                          // print(jsonTreeStore._opened_pathes);
+                          // print(jsonTreeStore.isOpened([...path, entry.key]));
+                          // print([...path, entry.key]);
+                        }
+                      }, child: Icon(
+                        jsonTreeStore.isOpened([...path, entry.key]) ? Icons.arrow_drop_up : Icons.arrow_drop_down,
+                      )),
+                      jsonTreeStore.isOpened([...path, entry.key])
+                        ? JsonTreeViewNode(
+                            jsonData: entry.value, path: [...path, entry.key])
+                        : Container(
+                          child: Text('---'),
+                        ),
+                ])
                   ),
                 ],
               ),
@@ -170,6 +168,7 @@ class JsonTreeViewNode extends StatelessWidget {
         padding: EdgeInsets.only(top: 0.0),
         child: w,
       );
+
       //   } else if (jsonData is List) {
       //     final w = Column(
       //     crossAxisAlignment: CrossAxisAlignment.start,
@@ -199,12 +198,15 @@ class JsonTreeViewNode extends StatelessWidget {
     } else {
       return GestureDetector(
         onTap: () {
-          // print('Value tapped: $jsonData');
-          print(path);
+          // Tried to listen to a value exposed with provider, from outside of the widget tree
+          jsonTreeStore.path = path;
         },
         child: Text(
           '$jsonData',
-          style: TextStyle(color: Colors.black),
+          style: TextStyle(
+            color: Colors.black,
+            backgroundColor: isOpenedKey ? Colors.red[200] : null,
+          ),
         ),
       );
     }
